4c14426106df541d9ed4999a06c13af1f902ef89
Payton Shaltis <shaltisp@gmail.com>
2022-12-07 14:42:13 -0500

    Adds more content to basic usage page, refines carousel styles.

--- docs/documentation/main.js
+++ docs/documentation/main.js
@@ -1,10 +1,21 @@
 import Freighter from "../../dist/Freighter.js";
 
-const freighter = new Freighter({
+const freighter1 = new Freighter({
   containerID: "carousel-1",
   resizingMethod: "stretch-scale",
   wrappingMethod: "none",
-  numItemsVisible: 2,
+  numItemsVisible: 3,
+  scrollBy: 2,
+  itemSpacing: 20,
+  itemHeight: 2,
+  itemWidth: 3,
+});
+
+const freighter2 = new Freighter({
+  containerID: "carousel-2",
+  resizingMethod: "stretch-scale",
+  wrappingMethod: "wrap-simple",
+  numItemsVisible: 3,
   scrollBy: 2,
   itemSpacing: 20,
   itemHeight: 2,
