d94387f36a585232b2a3d7099fff21cd0fdfd86c
Payton Shaltis <shaltisp@gmail.com>
2022-12-11 17:49:35 -0500

    Adds UMD and ES6 Webpack output options.

--- webpack-es6.config.js
+++ webpack-es6.config.js
@@ -2,8 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: "./js-output/Freighter.js",
+  experiments: {
+    outputModule: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "webpack-numbers.js",
+    filename: "FreighterES6.js",
+    globalObject: "this",
+    library: {
+      type: "module",
+    },
   },
 };
