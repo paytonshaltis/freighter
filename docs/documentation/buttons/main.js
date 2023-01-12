7d7090090dbca34ac75bd17483f7eefbec926c53
Payton Shaltis <shaltisp@gmail.com>
2023-01-12 17:33:11 -0500

    Completes the documentation content for all sections.

---
+++ docs/documentation/buttons/main.js
@@ -0,0 +1,15 @@
+import Freighter from "../../../dist/FreighterES6.js";
+
+const freighter1 = new Freighter("carousel-1", "stretch-scale", "none", {
+  numItemsVisible: 2,
+  scrollBy: 2,
+  itemSpacing: 30,
+  leftButtonStyles: {
+    position: "top",
+    borderTopLeftRadius: "10px",
+  },
+  rightButtonStyles: {
+    position: "bottom",
+    borderBottomRightRadius: "10px",
+  },
+});
